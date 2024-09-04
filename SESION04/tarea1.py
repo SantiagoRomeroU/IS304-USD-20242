355bb3e8c2b91759255ad289ef7f911eba2b64eb
SantiagoRomeroU <159743594+SantiagoRomeroU@users.noreply.github.com>
2024-09-04 09:25:59 -0500

    viernes 13

--- SESION04/tarea1.py
+++ SESION04/tarea1.py
@@ -1,2 +1,4 @@
-hey si si si
-hey hey hey si
+/*
+ * Crea una función que sea capaz de detectar si existe un viernes 13 en el mes y el año indicados.
+ * - La función recibirá el mes y el año y retornará verdadero o falso.
+ */
