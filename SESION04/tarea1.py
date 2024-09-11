6170ac30d4e23b8702ee9f76902dcdd9360f2134
SantiagoRomeroU <159743594+SantiagoRomeroU@users.noreply.github.com>
2024-09-10 23:59:28 -0500

    Update tarea1.py

--- SESION04/tarea1.py
+++ SESION04/tarea1.py
@@ -2,3 +2,6 @@
  * Crea una función que sea capaz de detectar si existe un viernes 13 en el mes y el año indicados.
  * - La función recibirá el mes y el año y retornará verdadero o falso.
  */ 
+def MetodoMain():
+    anio = int(input("Ingrese el año (1-9999): "))
+    mes = int(input("Ingrese el mes (1-12): "))
