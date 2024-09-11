b5319d0d29aa9542c588c58ac828568e32643f5b
SantiagoRomeroU <159743594+SantiagoRomeroU@users.noreply.github.com>
2024-09-11 00:10:19 -0500

    creacion de objeto e imprenta en pantalla

--- SESION04/tarea1.py
+++ SESION04/tarea1.py
@@ -6,7 +6,14 @@ import datetime
 def MetodoMain():
     ano = int(input("Ingrese el año (1-9999): "))
     mes = int(input("Ingrese el mes (1-12): "))
-    
+
+    comprobador = Viernes13(ano, mes)
+
+    if comprobador.es_viernes_13():
+        print(f"Sí hay un viernes 13 en {mes}/{ano}!")
+    else:
+        print(f"No hay un viernes 13 en {mes}/{ano}.")
+
 class Viernes13:
     #constructor de la clase
     def __init__(self, ano, mes):
@@ -15,7 +22,8 @@ class Viernes13:
 
     def es_viernes_13(self):
         if mes >= 1 and mes <= 12 and ano >= 1 and ano <= 9999:
-            #
+            fecha = datetime.date(self.ano, self.mes, 13)
+            return fecha.weekday() == 4
         else:
             return false
 
