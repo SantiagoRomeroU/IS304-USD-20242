796412c769aecc09cb332ab3a06a35663a2041a7
SantiagoRomeroU <159743594+SantiagoRomeroU@users.noreply.github.com>
2024-09-11 00:07:29 -0500

    creacion validacion

--- SESION04/tarea1.py
+++ SESION04/tarea1.py
@@ -1,15 +1,21 @@
-/*
- * Crea una función que sea capaz de detectar si existe un viernes 13 en el mes y el año indicados.
- * - La función recibirá el mes y el año y retornará verdadero o falso.
- */ 
+# Crea una función que sea capaz de detectar si existe un viernes 13 en el mes y el año indicados.
+# La función recibirá el mes y el año y retornará verdadero o falso.
+ 
 import datetime
 
 def MetodoMain():
-    anio = int(input("Ingrese el año (1-9999): "))
+    ano = int(input("Ingrese el año (1-9999): "))
     mes = int(input("Ingrese el mes (1-12): "))
     
 class Viernes13:
-    /*constructor*/
-    def __init__(self, anio, mes):
-        self.anio = anio
+    #constructor de la clase
+    def __init__(self, ano, mes):
+        self.ano = ano
         self.mes = mes
+
+    def es_viernes_13(self):
+        if mes >= 1 and mes <= 12 and ano >= 1 and ano <= 9999:
+            #
+        else:
+            return false
+
