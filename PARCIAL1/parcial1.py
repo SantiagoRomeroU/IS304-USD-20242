c534b8c715cdd8f6c3b0bb277ac770c1d962102c
SantiagoRomeroU <159743594+SantiagoRomeroU@users.noreply.github.com>
2024-09-18 08:30:59 -0500

    ejercicio a realizar

---
+++ PARCIAL1/parcial1.py
@@ -0,0 +1,2 @@
+// escriba un programa en python que permita clasificar triaungulos (isoceles,escaleno,equilatero, rectangulo) a partir de 3 datos flotantes o doubles ingresados desde el teclado.
+//definir y utilizar al menos una referencia de herencia que incluya encapsulamiento
