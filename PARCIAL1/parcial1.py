477fc2efa88617c215f42a8920ace50404a0ad3e
SantiagoRomeroU <159743594+SantiagoRomeroU@users.noreply.github.com>
2024-09-18 08:32:40 -0500

    Update parcial1.py

--- PARCIAL1/parcial1.py
+++ PARCIAL1/parcial1.py
@@ -1,2 +1,3 @@
 // escriba un programa en python que permita clasificar triaungulos (isoceles,escaleno,equilatero, rectangulo) a partir de 3 datos flotantes o doubles ingresados desde el teclado.
-//definir y utilizar al menos una referencia de herencia que incluya encapsulamiento
+// definir y utilizar al menos una referencia de herencia que incluya encapsulamiento
+// el programa debe repetirse continuamente hasta que 1 de los supuestos triangulos no lo sea
