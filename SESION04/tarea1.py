deeadea78d22e8a51b058440355df4ef077b1670
SantiagoRomeroU <159743594+SantiagoRomeroU@users.noreply.github.com>
2024-09-25 08:16:54 -0500

    Update tarea1.py

--- SESION04/tarea1.py
+++ SESION04/tarea1.py
@@ -1,3 +1,8 @@
+/*
+ * Crea una función que sea capaz de detectar si existe un viernes 13 en el mes y el año indicados.
+ * - La función recibirá el mes y el año y retornará verdadero o falso.
+ */
+
 import datetime
 
 def metodo_main():
